5b0736c57b6e778cd97e5a5ef9180fe05d2efa6c
Takayuki0924 <takapon0924@icloud.com>
2019-10-04 15:55:49 +0900

    Initial Commit

---
+++ Random1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random1
+//
+//  Created by 長野貴之 on 2019/10/04.
+//  Copyright © 2019 takayuki Nagano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
